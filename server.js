08e95d369fef0957ab0a2ac96b0f2d22c9504a47
josephredmond5 <josephredmond5@icloud.com>
2023-03-07 22:06:34 +0000

    added a comment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// This code creates a server that logs a message to the console every time a request is made to port 3000 of the local host.
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -7,3 +9,4 @@ console.log('request made');
 server.listen(3000, 'localhost', () => {
     console. log('listening for requests on port 3000');
 })
+
